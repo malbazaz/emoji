c1b00992450355980041e3391c8f164f99d7d773
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-02 18:32:11 -0400

    update slug mistake and working on gimoji controller.

--- app/controllers/gimojis_controller.rb
+++ app/controllers/gimojis_controller.rb
@@ -2,13 +2,52 @@ class GimojisController < ApplicationController
 
 	get '/gimojis' do
 
-	  if !!session[:user_id]
-	    @user = User.find_by_id(session[:user_id])
-	    erb :'gimojis/gimojis'
-	  else
-	    redirect '/login'
-	  end
+		if !!session[:user_id]
+			@user = User.find_by_id(session[:user_id])
+			erb :'gimojis/gimojis'
+		else
+			redirect '/login'
+		end
 	end 
+
+	get '/gimojis/new' do
+		if !!session[:user_id]
+			erb :'gimojis/create'
+		else
+			redirect '/login'
+		end
+	end
+
+	post '/gimojis' do
+
+		if params[:gimoji][:name] == "" || params[:gimoji][:tag] == ""
+			redirect to '/gimojis/new'
+		else
+			@gimoji = gimoji.create(name: params[:gimoji][:name], tag: params[:gimoji][:tag])
+			@gimoji.user_id = session[:user_id]
+			@gimoji.save
+			redirect to "/gimojis/#{@gimoji.slug}"
+ 		end
+ 	end
+
+get '/gimojis/:slug' do
+	if !!session[:user_id]
+		@gimoji = gimoji.find_by_id(params[:id])
+		erb :'gimojis/show'
+	else
+		redirect '/login'
+	end
+end
+
+get '/gimojis/:slug/edit' do
+	if !!session[:user_id]
+		@gimoji = gimoji.find_by_id(params[:id])
+		erb :'gimojis/edit'
+	else
+		redirect '/login'
+	end
+end
+
 
 	
 end

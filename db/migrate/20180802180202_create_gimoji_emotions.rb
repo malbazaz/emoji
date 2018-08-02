d3f664ab056967a3fe788e0f824e8d726cba4620
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-02 14:05:59 -0400

    update the migration tables for emotions and gimoji-emotions.

--- db/migrate/20180802180202_create_gimoji_emotions.rb
+++ db/migrate/20180802180202_create_gimoji_emotions.rb
@@ -1,4 +1,8 @@
 class CreateGimojiEmotions < ActiveRecord::Migration
-  def change
-  end
+  	def change
+  	  	create_table :gimoji_emotions do |t|
+    		t.integer :gimoji_id
+    		t.integer :emotion_id
+    	end
+  	end
 end

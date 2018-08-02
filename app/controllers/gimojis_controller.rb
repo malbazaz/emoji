e420845af83a52f927eb899a723ad56a5f8d5c43
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-02 19:47:12 -0400

    finish gimojis controller.

--- app/controllers/gimojis_controller.rb
+++ app/controllers/gimojis_controller.rb
@@ -23,7 +23,7 @@ class GimojisController < ApplicationController
 		if params[:gimoji][:name] == "" || params[:gimoji][:tag] == ""
 			redirect to '/gimojis/new'
 		else
-			@gimoji = gimoji.create(name: params[:gimoji][:name], tag: params[:gimoji][:tag])
+			@gimoji = Gimoji.create(name: params[:gimoji][:name], tag: params[:gimoji][:tag])
 			@gimoji.user_id = session[:user_id]
 			@gimoji.save
 			redirect to "/gimojis/#{@gimoji.slug}"
@@ -32,7 +32,7 @@ class GimojisController < ApplicationController
 
 get '/gimojis/:slug' do
 	if !!session[:user_id]
-		@gimoji = gimoji.find_by_id(params[:id])
+		@gimoji = Gimoji.find_by_id(params[:id])
 		erb :'gimojis/show'
 	else
 		redirect '/login'
@@ -41,13 +41,39 @@ end
 
 get '/gimojis/:slug/edit' do
 	if !!session[:user_id]
-		@gimoji = gimoji.find_by_id(params[:id])
+		@gimoji = Gimoji.find_by_id(params[:id])
 		erb :'gimojis/edit'
 	else
 		redirect '/login'
 	end
 end
 
+patch '/gimojis/:slug' do
+	if params[:gimoji][:name] == "" || params[:gimoji][:tag] == ""
+		redirect "/gimojis/#{params[:slug]}/edit"
+	end
+	@gimoji = Gimoji.find_by_id(params[:id])
+	@gimoji.name = params[:gimoji][:name]
+	@gimoji.save
+	erb :'gimojis/show'
+end
 
+delete '/gimojis/:slug/delete' do
+    #binding.pry
+    @gimoji = Gimoji.find_by_id(params[:id])
+    if !session[:user_id]
+    	redirect to "/login"
+    end
+    if @gimoji.user_id == session[:user_id]
+    #binding.pry
+    @gimoji.delete
+    redirect to "/gimojis"
+else
+	redirect to "/login"
+end
+end
+
+
+end
 	
 end

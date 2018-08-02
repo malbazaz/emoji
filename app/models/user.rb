0a2cac8cebe8f0c28266ca0baeb63f2a7ff92aeb
malbazaz <39787850+malbazaz@users.noreply.github.com>
2018-08-02 10:56:36 -0400

    add basic user class with slug

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :gimojis
+  
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(sluggedstring)
+    @array1 = []
+    self.all.find do |inst|
+        inst.slug == sluggedstring
+    end
+  end
+
+end
